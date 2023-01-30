d75c6f031ba0710f8ebc08858cce6d691a1e6469
Jakub Boczkowski <jakub.boczkowski@ceneo.pl>
2023-01-30 12:50:05 +0100

    add tests

--- GildedRose.Tests/GildedRoseTests.cs
+++ GildedRose.Tests/GildedRoseTests.cs
@@ -29,13 +29,25 @@ namespace GildedRose.Tests
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                 new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
                 new Item
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 15,
                     Quality = 20
                 },
-                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
             };
 
             private List<Item> Items2 = new List<Item>
@@ -44,13 +56,25 @@ namespace GildedRose.Tests
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                 new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
                 new Item
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 15,
                     Quality = 20
                 },
-                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
             };
 
             public GildedRoseTestsFixture()
